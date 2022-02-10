bd9a8792a11d8e015be38c087d5741e42a8fb815
Alan Tang <alantang888@users.noreply.github.com>
2022-02-10 14:16:51 +0000

    Init commit
    POC work. It can kill pod in `Terminating` when that last for 5 mins

---
+++ cmd/k8s-terminating-pod-killer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	k8sConfig, err := rest.InClusterConfig()
+	if err != nil {
+		log.Printf("Get in cluster k8sConfig error: %s\n\n", err.Error())
+		log.Println("Will try connect to local 8001 (kubectl proxy)")
+		k8sConfig = &rest.Config{
+			Host: "http://127.0.0.1:8001",
+		}
+	}
+
+	zero := int64(0)
+
+	clientset, err := kubernetes.NewForConfig(k8sConfig)
+	pods, _ := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
+	for _, p := range pods.Items {
+		if p.ObjectMeta.DeletionTimestamp != nil && time.Now().Unix()-p.ObjectMeta.DeletionTimestamp.Unix() > (5*60) {
+			fmt.Printf("Killing: %s/%s\n", p.Namespace, p.Name)
+			err = clientset.CoreV1().Pods(p.Namespace).Delete(context.Background(), p.Name, metav1.DeleteOptions{GracePeriodSeconds: &zero})
+			if err != nil {
+				fmt.Printf("Got error when kill %s/%s. Err: %s\n", p.Namespace, p.Name, err.Error())
+			}
+		}
+	}
+}
